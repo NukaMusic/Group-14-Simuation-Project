9388e89965d4fde733a6be7eb9f61683a56065c5
NukaMusic <NukaMiettinen@gmail.com>
2021-10-21 21:32:49 +0100

    Code tidying and import file
    Removed redundant code and functionized graphing.

--- Main Code.py
+++ Main Code.py
@@ -1,39 +1,25 @@
-'''Simulation
+"""Simulation
 
 Written by Group 14:
 Nuka Miettinen Langgård
 Conor Shannon
 2021
-'''
+"""
 
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import time  # for debugging and optimization
+from vars_init_conditions import *
 
 starttime = time.time()
 
 print(time.time() - starttime)
 
-database_file = 'velocityCMM3.dat'  # import file object velocity field must have same minimum and maximum bounds or the grid will stretch
-
-pos = np.genfromtxt(database_file, usecols=(0, 1))  # position array
-vel = np.genfromtxt(database_file, usecols=(2, 3))  # velocity array
-row_length = np.sqrt(len(pos)).astype(int) 
+pos = np.genfromtxt(vel_file, usecols=(0, 1))  # position array
+vel = np.genfromtxt(vel_file, usecols=(2, 3))  # velocity array
+row_length = np.sqrt(len(pos)).astype(int)
 column_length = row_length  # currently assumes square
-
-# Domain size
-x_min = -1
-x_max = 1
-y_min = -1
-y_max = 1
-
-t_max = 0.08  # simulation time in seconds
-dt = 0.0005  # step size
-N = 2 ** 21  # Number of particles
-D = 0.01  # diffusivity
-Nx = 256  # Euler grid size x
-Ny = 256  # Euler grid size y
 
 x = np.random.uniform(x_min, x_max, size=N)  # init x-positions
 y = np.random.uniform(y_min, y_max, size=N)  # init y-positions
@@ -45,85 +31,71 @@ blue = np.full(N, 'b')  # Array of blue for where function
 red = np.full(N, 'r')  # Array of red for where function
 cmap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap', ['r', 'lime', 'b'], 256)  # colormap for graphing
 
-# initial conditions
-# 1: 1D problem
-# 2: middle patch
-# 3: side patch
-init_type = 3
-
-# Graphing type
-# 1: Particles
-# 2: Concentation field
-graph_type = 2
-
 if init_type == 2:
     phi = np.where(np.sqrt(x ** 2 + y ** 2) < 0.3, phi1, phi0)
 
 if init_type == 3:
     phi = np.where(x < 0, phi1, phi0)
 
-
-# create a mesh and find the average phi values within it
+# Create a mesh and find the average phi values within it
 def getavrphimesh(x, y):
     x_gran = np.round((x - x_min) / (x_max - x_min) * (Nx-1)).astype(int)  # figures out which grid square (granular
     y_gran = np.round((y - y_min) / (y_max - y_min) * (Ny-1)).astype(int)  # coordinate) each point fits into
     grancoord = np.column_stack((x_gran, y_gran))  # array of each point's granular coordinate
     unq, ids, count = np.unique(grancoord, return_inverse=True, return_counts=True, axis=0)
     avrphi = np.bincount(ids, phi)/count
-    avrphi = np.flipud(np.rot90(np.reshape(avrphi, [Nx, Ny])))
+    avrphi = np.rot90(np.reshape(avrphi, [Nx, Ny]))
     return avrphi
 
 
-if graph_type == 1:
-    for i in range(N):
-        col = np.where(phi == 1, blue, red)  # create array of colours for each point
-    plt.scatter(x, y, color=col, s=0.1)
-    plt.show()
+# Visualize the data
+def visualize(viz_type):
+    if viz_type == 1:
+        for i in range(N):
+            col = np.where(phi == 1, blue, red)  # create array of colours for each point
+        plt.scatter(x, y, color=col, s=0.1)
+        plt.show()
+
+    if viz_type == 2:
+        avphi = getavrphimesh(x, y)
+        plt.imshow(avphi, interpolation='nearest', cmap=cmap,
+                   extent=(x_min, x_max, y_min, y_max))  # interpolate = ?, cmap = colour map, extent changes graph size
+        plt.colorbar()  # colour map legend
+        plt.show()  # plot it!
 
 
-if graph_type == 2:
-    avphi = getavrphimesh(x, y)
-    plt.imshow(avphi, interpolation='nearest', cmap=cmap, origin='lower', extent=(x_min, x_max, y_min, y_max))  # interpolate = ?, cmap = colour map, origin = 'Lower' -> (0,0) at bottom left, extent changes graph size
-    plt.colorbar()  # colour map legend
-    plt.show()  # plot it!
+visualize(viz_type)
 
 print(time.time() - starttime)
 
-def get_velocities(x, y): # given a coordinate, tells us what nearest velocity vector is
+
+def get_velocities(x, y):  # given a coordinate, tells us what nearest velocity vector is
     x_coordinates = np.round((x - np.amin(x)) / (np.amax(x) - np.amin(x)) * (row_length-1)).astype(int)  # same indexing
     y_coordinates = np.round((y - np.amin(y)) / (np.amax(y) - np.amin(y)) * (row_length-1)).astype(int)  # as avrphi function
-    x_velocities = np.empty(shape=N) # empty arrays to receive velocity data
+    x_velocities = np.empty(shape=N)  # empty arrays to receive velocity data
     y_velocities = np.empty(shape=N)
     for i in range(N): # turns our two vel arrays into a 1D array
         velocity_index = y_coordinates[i] + x_coordinates[i] * row_length
         x_velocities[i] = vel[velocity_index][0]
         y_velocities[i] = vel[velocity_index][1]
-    return x_velocities, y_velocities 
+    return x_velocities, y_velocities
 
 
 print(time.time() - starttime)
 
-
 for i in np.arange(0, (t_max+dt), dt): 
-    v_x, v_y = get_velocities(x, y)
-    x += v_x * dt + np.sqrt(2 * D * dt) * np.random.normal(0, 1, size=N)  # Lagrange Diffusion and advection
-    y += v_y * dt + np.sqrt(2 * D * dt) * np.random.normal(0, 1, size=N)  # Lagrange Diffusion and advection
-    #Walls
+    if vel_type == 1:
+        v_x, v_y = get_velocities(x, y)
+        x += v_x * dt
+        y += v_y * dt
+    x += np.sqrt(2 * D * dt) * np.random.normal(0, 1, size=N)  # Lagrange Diffusion and advection
+    y += np.sqrt(2 * D * dt) * np.random.normal(0, 1, size=N)  # Lagrange Diffusion and advection
+    # Walls
     x = np.where(x > x_max, 2 * x_max - x, x)  # if point is beyond wall, update
     x = np.where(x < x_min, 2 * x_min - x, x)  # position to bounce off wall as
     y = np.where(y > y_max, 2 * y_max - y, y)  # far as it went beyond the wall
     y = np.where(y < y_min, 2 * y_min - y, y)
 
-print(time.time() - starttime)
-
-if graph_type == 1:
-    plt.scatter(x, y, color=col, s=0.1)
-    plt.show()
-
-if graph_type == 2:
-    avphi = getavrphimesh(x, y)
-    plt.imshow(avphi, interpolation='nearest', cmap=cmap, origin='lower', extent=(x_min, x_max, y_min, y_max))  # interpolate = ?,
-    plt.colorbar()  # colour map legend
-    plt.show()  # plot it!
+visualize(viz_type)
 
 print(time.time() - starttime)
