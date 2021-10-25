a0ed0ad68b8591c9c8b25326b93ed0753767deba
NukaMusic <NukaMiettinen@gmail.com>
2021-10-26 00:06:53 +0100

    Small fixes and clarifications

--- Main_Code.py
+++ Main_Code.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 import time  # for debugging and optimization
 import multiprocessing as mp
 
+
 class Simulation:
     """
         Runs simulation.
@@ -22,27 +23,25 @@ class Simulation:
         database_file: Path to data.
     """
 
-    database_file: str()
-    
     def __init__(self, database_file: str):
         """
         Init class.
         """
         self.debug = False
         self.database_file = database_file
-    
-    def start_simulation(self, x_min, x_max, y_min, y_max, t_max, dt, exp, D, Nx, Ny, init_type, viz_type, vel_type):  
+
+    def start_simulation(self, x_min, x_max, y_min, y_max, t_max, dt, exp, D, Nx, Ny, init_type, viz_type, vel_type):
         """
         Start simulation; option to change default values.
         """
         starttime = time.time()
-        
+
         if self.debug:
             print(time.time() - starttime)
-         
+
         print("[" + mp.current_process().name + "] Simulation running...")
-        
-        N = 2**exp
+
+        N = 2 ** exp
         x = np.random.uniform(x_min, x_max, size=N)  # initial x-positions
         y = np.random.uniform(y_min, y_max, size=N)  # initial y-positions
         pos = np.genfromtxt(self.database_file, usecols=(0, 1))  # position array for velocity field
@@ -53,45 +52,49 @@ class Simulation:
         zeros = np.zeros(N)  # Array of zeros for where function
         blue = np.full(N, 'b')  # Array of blue for where function
         red = np.full(N, 'r')  # Array of red for where function
-        cmap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap', ['r', 'lime', 'b'], 256)  # colormap for graphing
-        
+        cmap = mpl.colors.LinearSegmentedColormap.from_list('my_colormap', ['r', 'lime', 'b'])  # colormap for graphing
+
         oneD_ref = np.genfromtxt('reference_solution_1D.dat')
-        
+
         if init_type == 1:
             phi = np.where(x <= 0, ones, zeros)
             y_min = -0.001
             y_max = 0.001
             Ny = 1
+            D = 0.1
             vel_type = 0
-            
+            t_max = 0.2
+
         if init_type == 2:
             phi = np.where(np.sqrt(x ** 2 + y ** 2) < 0.3, ones, zeros)
-            
+
         if init_type == 3:
             phi = np.where(x < 0, ones, zeros)
-            
+
         # Velocity data position resolution (distance between velocity field points
         x_posres = (np.max(pos[:, 0]) - np.min(pos[:, 0])) / (len(np.unique(pos[:, 0]).astype(int)) - 1)
         y_posres = (np.max(pos[:, 1]) - np.min(pos[:, 1])) / (len(np.unique(pos[:, 1]).astype(int)) - 1)
-        
-        maxdist = np.sqrt(x_posres ** 2 + y_posres ** 2)  # maximum allowable distance for a particle to be from a vel coord
-        
+
+        maxdist = np.sqrt(
+            x_posres ** 2 + y_posres ** 2)  # maximum allowable distance for a particle to be from a vel coord
+
         if self.debug:
             print(time.time() - starttime)
-         
+
         # Create a mesh and find the average phi values within it
         def getavrphimesh(x, y):
-            x_gran = np.round((x - x_min) / (x_max - x_min) * (Nx-1)).astype(int)  # figures out which grid square (granular
-            y_gran = np.round((y - y_min) / (y_max - y_min) * (Ny-1)).astype(int)  # coordinate) each point fits into
+            x_gran = np.round((x - x_min) / (x_max - x_min) * (Nx - 1)).astype(
+                int)  # figures out which grid square (granular
+            y_gran = np.round((y - y_min) / (y_max - y_min) * (Ny - 1)).astype(int)  # coordinate) each point fits into
             grancoord = np.column_stack((x_gran, y_gran))  # array of each point's granular coordinate
             unq, ids, count = np.unique(grancoord, return_inverse=True, return_counts=True, axis=0)
-            avrphi = np.bincount(ids, phi)/count
+            avrphi = np.bincount(ids, phi) / count
             avrphi = np.rot90(np.reshape(avrphi, [Nx, Ny]))
             return avrphi
-         
+
         if self.debug:
             print(time.time() - starttime)
-        
+
         def get_velocities(x, y):  # given a coordinate, tells us what nearest velocity vector is
             distance, index = spatial.cKDTree(pos).query(np.column_stack((x, y)), workers=-1)
             x_velocities = vel[index][:, 0]
@@ -99,11 +102,10 @@ class Simulation:
             x_velocities = np.where(distance > maxdist, zeros, x_velocities)
             y_velocities = np.where(distance > maxdist, zeros, y_velocities)
             return x_velocities, y_velocities
-        
-        
+
         # Visualize the data
         def visualize(init_type, viz_type):
-            
+
             if init_type == 1:
                 avphi = getavrphimesh(x, y)
                 plt.plot(oneD_ref[:, 0], oneD_ref[:, 1], color='r')
@@ -114,35 +116,38 @@ class Simulation:
                 plt.xlabel('x')
                 plt.ylabel('Concentration, ϕ ')
                 plt.show()
-                
+
             if init_type == 2 or init_type == 3:
                 if viz_type == 1:
                     for i in range(N):
                         col = np.where(phi == 1, blue, red)  # create array of colours for each point
                     plt.scatter(x, y, color=col, s=0.1)
-                    plt.title('2D Particle Location Visualisation at '+str(round(t/dt)*dt)+' s', fontdict=None, loc='center', pad=20)  # Plot Titles
+                    plt.title('2D Particle Location Visualisation at ' + str(round(t / dt) * dt) + ' s', fontdict=None,
+                              loc='center', pad=20)  # Plot Titles
                     plt.xlabel('x')
                     plt.ylabel('y')
                     plt.show()
-        
+
                 if viz_type == 2:
                     avphi = getavrphimesh(x, y)
                     plt.imshow(avphi, interpolation='nearest', cmap=cmap,
-                               extent=(x_min, x_max, y_min, y_max))  # interpolate = ?, cmap = colour map, extent changes graph size
+                               extent=(x_min, x_max, y_min,
+                                       y_max))  # interpolate = ?, cmap = colour map, extent changes graph size
                     plt.colorbar(label='Concentration, ϕ')  # colour map legend
-                    plt.title('2D Particle Concentration Representation at '+str(round(t/dt)*dt)+' s', fontdict=None, loc='center', pad=20)  # Plot Titles
+                    plt.title('2D Particle Concentration Representation at ' + str(round(t / dt) * dt) + ' s',
+                              fontdict=None, loc='center', pad=20)  # Plot Titles
                     plt.xlabel('x')
                     plt.ylabel('y')
                     plt.show()  # plot it!
-            
+
         if init_type == 2 or init_type == 3:
             visualize(init_type, viz_type)
-        
+
         if self.debug:
             print(time.time() - starttime)
-        
-        for i in np.arange(0, (t_max+dt), dt):
-            if vel_type == 1:
+
+        for step in np.arange(dt, (t_max + dt), dt):
+            if vel_type:
                 v_x, v_y = get_velocities(x, y)
                 x += v_x * dt
                 y += v_y * dt
@@ -154,10 +159,10 @@ class Simulation:
             y = np.where(y > y_max, 2 * y_max - y, y)  # far as it went beyond the wall
             y = np.where(y < y_min, 2 * y_min - y, y)
             t += dt  # t for second title
-        
+
         visualize(init_type, viz_type)
-        
+
         if self.debug:
             print(time.time() - starttime)
-        
+
         print("[" + mp.current_process().name + "] Simulation done.")
--- gui.py
+++ gui.py
@@ -6,11 +6,13 @@ dpg.create_context()
 dpg.create_viewport(title="Simulation", width=900, height=800)
 dpg.setup_dearpygui()
 
+
 def call_simulation(path, a, b, c, d, e, f, g, h, i, j, k, l, m):
     temp = Main_Code.Simulation(path)
-    temp.start_simulation(a, b, c, d, e, f, g, h, i, j, k , l, m)
+    temp.start_simulation(a, b, c, d, e, f, g, h, i, j, k, l, m)
 
-def sim_callback(sender, data):
+
+def sim_callback():
     ctx = mp.get_context("spawn")
     p = ctx.Process(target=call_simulation, args=(
         dpg.get_value("path"), dpg.get_value("min_x"), 
@@ -20,10 +22,11 @@ def sim_callback(sender, data):
         float(dpg.get_value("diff")), dpg.get_value("euler_x"), dpg.get_value("euler_y"),
         dpg.get_value("init_type"), dpg.get_value("viz_type"), dpg.get_value("vel_type")))
     p.start()
-    
+
+
 # Creating Labels for the Input Parameters
 with dpg.window(label="Parameters", width=800):
-    dpg.add_text("Location of Simulation Data")
+    dpg.add_text("File name of velocity field")
     dpg.add_input_text(tag="path", default_value="velocityCMM3.dat", label=" (.dat)")
     dpg.add_text("Domain")
     dpg.add_input_int(tag="min_x", default_value=-1, label=" (min. x)")
@@ -33,20 +36,20 @@ with dpg.window(label="Parameters", width=800):
     dpg.add_text("Euler Grid Size")
     dpg.add_input_int(tag="euler_x", default_value=64, label=" (x)")
     dpg.add_input_int(tag="euler_y", default_value=64, label=" (y)")
-    dpg.add_text("Maximum Runtime")
+    dpg.add_text("Simulated time")
     dpg.add_input_text(tag="time", decimal=True, default_value=0.2, label=" second(s)")
     dpg.add_text("Step Size")
     dpg.add_input_text(tag="step_size", decimal=True, default_value=0.0005)
     dpg.add_text("# of Particles (2^(your_value_below))")
-    dpg.add_input_int(tag="num_particles", default_value=17, label=" (exp.)") # dpg.add_drag_int for a slider
+    dpg.add_input_int(tag="num_particles", default_value=17, label=" (exp.)")  # dpg.add_drag_int for a slider
     dpg.add_text("Diffusivity")
     dpg.add_input_text(tag="diff", default_value=0.01, decimal=True)
-    dpg.add_text("Init Type: 1 for 1D problem (overrides y_min, y_max, Ny, and vel_type, 2 for middle patch, 3 for side patch)")
+    dpg.add_text("Init Type: 1 for 1D problem (overrides y_min, y_max, Ny, D, t_max and vel_type), 2 for middle patch, 3 for side patch")
     dpg.add_input_int(tag="init_type", default_value=1, label=" Initial Cond.")
     dpg.add_text("Vis Type: 1 for particles, 2 for Concentration field")
     dpg.add_input_int(tag="viz_type", default_value=1, label=" Vis. Display ")
-    dpg.add_text("Vel Type: 0 for No velocity field, 1 for the previously defined velocity") #Turn it to boolean -maybe
-    dpg.add_input_int(tag="vel_type", default_value=0, label=" Vel. Field Conditions")
+    dpg.add_text("Vel Type: False(0) for No velocity field, True(1) for the previously defined velocity field")
+    dpg.add_input_int(tag="vel_type", default_value=True, label=" Vel. Field Conditions")  # (change this to be a true and false button maybe?)
     dpg.add_text("Done?")
     dpg.add_button(label="Run Simulation", callback=sim_callback)
 
